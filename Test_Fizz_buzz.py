4984348c8ebbe21a50b7a0d9b3432cf383ee9719
awa <awadoubia2006@gmail.com>
2025-10-14 10:29:01 +0200

    Faill: affiche retourne None

--- Test_Fizz_buzz.py
+++ Test_Fizz_buzz.py
@@ -4,9 +4,10 @@ from Fizz_buzz import fizz_buzz
 
 class TestFizzBuzz(unittest.TestCase):
 
-    def test_affiche_retourne_chaine(self):
+    def test_affiche_retourne_rien(self):
         fb = fizz_buzz()
         resultat = fb.affiche()
         self.assertEqual(resultat, "")
+
 if __name__ == "__main__":
     unittest.main()
