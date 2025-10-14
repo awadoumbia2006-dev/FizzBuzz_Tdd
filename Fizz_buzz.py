bfbc2e989bcffa445b878f71022fb13df7a06d66
awa <awadoubia2006@gmail.com>
2025-10-14 11:20:43 +0200

    Implémentation complète : FizzBuzz avec FrisBee pour 15

--- Fizz_buzz.py
+++ Fizz_buzz.py
@@ -2,8 +2,16 @@ class fizz_buzz:
     def __init__(self):
         pass
 
+
     def affiche(self):
         resultat = ""
-        for i in range(1, 101):  # on génère tous les nombres
-            resultat += str(i)
+        for i in range(1, 101):
+            if i % 15 == 0:
+                resultat += "FrisBee"
+            elif i % 3 == 0:
+                resultat += "Fizz"
+            elif i % 5 == 0:
+                resultat += "Buzz"
+            else:
+                resultat += str(i)
         return resultat
--- Test_Fizz_buzz.py
+++ Test_Fizz_buzz.py
@@ -9,8 +9,13 @@ class TestFizzBuzz(unittest.TestCase):
         resultat = fb.affiche()
     # 3 est un multiple de 3 → doit être remplacé
         self.assertIn("Fizz", resultat)
-        self.assertNotIn("3", resultat)
+        #self.assertNotIn("3", resultat)
 
+        self.assertEqual(fb.affiche(),"12Fizz4BuzzFizz78FizzBuzz11Fizz1314FrisBee1617" \
+        "Fizz19BuzzFizz2223FizzBuzz26Fizz2829FrisBee3132Fizz34BuzzFizz3738FizzBuzz41" \
+        "Fizz4344FrisBee4647Fizz49BuzzFizz5253FizzBuzz56Fizz5859FrisBee6162Fizz64BuzzFizz" \
+        "6768FizzBuzz71Fizz7374FrisBee7677Fizz79BuzzFizz8283FizzBuzz86Fizz8889FrisBee9192" \
+        "Fizz94BuzzFizz9798FizzBuzz")
 
 if __name__ == "__main__":
     unittest.main()
