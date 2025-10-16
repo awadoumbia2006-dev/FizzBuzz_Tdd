df112dd2df67cfc75a84a34e40ff67de49cfb9eb
awa <awadoubia2006@gmail.com>
2025-10-16 18:36:10 +0200

    Fail: Cryptage.crypt() ne peut pas prendre 3 paramètres

--- Test_Cryptage.py
+++ Test_Cryptage.py
@@ -8,5 +8,11 @@ class TestCryptage(unittest.TestCase):
         resultat = c.crypt("abc")
         self.assertEqual(resultat, "bcd")  # chaque lettre décalée de 1
 
+
+    def test_crypt_avec_pas(self):
+        c = Cryptage()  # création de l'objet c
+        resultat = c.crypt("abc", 2)
+        self.assertEqual(resultat, "cde2")  # décalé de 2 + ajout du pas
+
 if __name__ == "__main__":
-    unittest.main()
+     unittest.main()
