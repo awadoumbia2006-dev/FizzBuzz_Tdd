ae9eb94dd404a525cf3541ad50901a1f4aadf331
awa <awadoubia2006@gmail.com>
2025-10-16 18:41:51 +0200

    Fin de Cryptage avec pas

--- Cryptage.py
+++ Cryptage.py
@@ -5,9 +5,16 @@ class Cryptage:
         self.caracteres = string.ascii_letters + string.punctuation + string.digits + " "
 
 
-    def crypt(self, message):
+    def crypt(self, message, pas=1):
         resultat = ""
         for char in message:
             index = self.caracteres.index(char)
-            resultat += self.caracteres[(index + 1) % len(self.caracteres)]
+            resultat += self.caracteres[(index + pas) % len(self.caracteres)]
+
+        # N’ajoute le pas à la fin que si l’utilisateur l’a fourni explicitement
+        if pas != 1:
+            resultat += str(pas)
+
         return resultat
+
+
