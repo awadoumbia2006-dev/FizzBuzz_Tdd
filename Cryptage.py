8a235aacb13e5780ba0837cd354c1037493cfbbf
awa <awadoubia2006@gmail.com>
2025-10-16 18:58:14 +0200

    Fin de decrypt

--- Cryptage.py
+++ Cryptage.py
@@ -2,20 +2,36 @@ import string
 
 class Cryptage:
     def __init__(self):
+        # Table de caractères
         self.caracteres = string.ascii_letters + string.punctuation + string.digits + " "
-
 
     def crypt(self, message, pas=1):
         resultat = ""
         for char in message:
-            index = self.caracteres.index(char)
-            resultat += self.caracteres[(index + pas) % len(self.caracteres)]
-
-        # N’ajoute le pas à la fin que si l’utilisateur l’a fourni explicitement
+            if char in self.caracteres:
+                index = self.caracteres.index(char)
+                resultat += self.caracteres[(index + pas) % len(self.caracteres)]
+            else:
+                resultat += char  # si caractère non trouvé (sécurité)
+        
+        # Ajoute le pas seulement s’il a été fourni explicitement
         if pas != 1:
             resultat += str(pas)
-
         return resultat
-    
-    
 
+    def decrypt(self, message):
+        # Récupère le pas à la fin si c’est un chiffre
+        if message[-1].isdigit():
+            pas = int(message[-1])
+            message = message[:-1]
+        else:
+            pas = 1
+
+        resultat = ""
+        for char in message:
+            if char in self.caracteres:
+                index = self.caracteres.index(char)
+                resultat += self.caracteres[(index - pas) % len(self.caracteres)]
+            else:
+                resultat += char
+        return resultat
